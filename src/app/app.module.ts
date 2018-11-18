74cddcbed92813a93ffea4533cb5783c980b3c4e
RomaDemiurg <demiurgru@yandex.ru>
2018-11-18 22:15:49 +0200

    Add AngularFireFeatures: firestore, auth, storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatListModule} from '@angular/material';
+import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule, MatListModule } from '@angular/material';
 import { AverageRGBComponent } from './average-rgb/average-rgb.component';
 import { MainRGBComponent } from './main-rgb/main-rgb.component';
 import { VibrantComponent } from './vibrant/vibrant.component';
@@ -12,6 +12,26 @@ import { ImageCropperComponent } from './image-cropper/image-cropper.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
+
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+const AngularMaterialFeatures = [
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
+]
+
+const AngularFireFeatures = [
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+]
 
 @NgModule({
   declarations: [
@@ -24,14 +44,9 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    ...AngularFireFeatures,
     BrowserAnimationsModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    MatListModule
+    ...AngularMaterialFeatures
   ],
   providers: [],
   bootstrap: [AppComponent]
