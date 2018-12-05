fcfa8e7b2fc3f085ef7a6fe28296dc5cf1fe8d51
RomaDemiurg <demiurgru@yandex.ru>
2018-12-05 22:16:26 +0200

    add static data

---
+++ src/app/vibrant/static-data.ts
@@ -0,0 +1,10 @@
+export const folders = [
+    { avatar: 'assets/img/avatars/1.jpg', name: 'Photos', updated: new Date('1/1/16') },
+    { avatar: 'assets/img/avatars/2.jpg', name: 'Recipes', updated: new Date('1/17/16') },
+    { avatar: 'assets/img/avatars/3.jpg', name: 'Work', updated: new Date('1/28/16') }
+]
+
+export const notes = [
+    { name: 'Vacation Itinerary', updated: new Date('2/20/16') },
+    { name: 'Kitchen Remodel', updated: new Date('1/18/16') }
+]
